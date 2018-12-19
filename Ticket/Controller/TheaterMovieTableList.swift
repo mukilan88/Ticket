318d1d1729f171772a765d99ddc25f56347c5cac
Swifty <arunswifty.com>
2018-12-19 21:07:34 +0530

    19/12/18 day final out put movie select theatre list show

--- Ticket/Controller/TheaterMovieTableList.swift
+++ Ticket/Controller/TheaterMovieTableList.swift
@@ -12,10 +12,10 @@ import RealmSwift
 
 class TheaterMovieTableList: UIViewController,UITableViewDataSource, UITableViewDelegate {
     //var for the list of movie or theater to show
-    var theaterMovieList : Results<Theater>!
+    
     var movieTheaterList : Results<Movie>!
     
-    var theaterList = Array<Any>()
+    var theaterList = List<Theater>()
     
     var movieID = ""
     var theaterID = ""
@@ -28,44 +28,21 @@ class TheaterMovieTableList: UIViewController,UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
         let filter_mov = NSPredicate.init(format: "movieID == %@", movieID)
         let mov = MyRealm.objects(Movie.self).filter(filter_mov)
         
-        print("***********  ",mov[0].theaterID)
-        
-        print("!!!!!!!!!!!!  ",mov[0].theaterID.count)
-      
-        
         while i < mov[0].theaterID.count{
 
-            print(">>>>>>>>>>>>>>>>  ",mov[0].theaterID[i])
-//
             let filter_thr = NSPredicate.init(format: "theaterID == %@", mov[0].theaterID[i])
             let thr = MyRealm.objects(Theater.self).filter(filter_thr)
-//
-
-            
-            
-            self.theaterList.append(thr)
-//
+            self.theaterList.append(thr[0])
             i = i + 1
-//
+            
         }
       
-       
-//        theaterArray.append(mov[0].theaterID)
-//
-        print("***********  ",self.theaterMovieList)
-//
-//        let filter_theater = NSPredicate.init(format: "theaterID == %@", theaterID)
-//        let movTheater = MyRealm.objects(Theater.self).filter(filter_theater)
-        
-        //print("***********  ",movTheater[0].movieID)
-//        maineTMTable.dataSource = self
-//        maineTMTable.delegate = self
-//        maineTMTable.reloadData()
+        maineTMTable.dataSource = self
+        maineTMTable.delegate = self
+        maineTMTable.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -79,7 +56,7 @@ class TheaterMovieTableList: UIViewController,UITableViewDataSource, UITableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "TheaterMovieCell", for: indexPath) as! TheaterMovieCell
-        let dict = self.theaterMovieList[indexPath.row]
+        let dict = self.theaterList[indexPath.row]
         cell.theaterMovieNameCell.text = dict.theaterNameRealm ?? ""
         cell.theaterMovieAddCell.text = dict.addressRealm ?? ""
         cell.theaterMovieTimeCell.text = String(dict.timeRealm)
